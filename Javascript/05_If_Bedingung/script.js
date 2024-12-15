a9d248a1360527bbfa0b62fcb6fcb525b12c8ff2
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-12-15 17:42:24 +0100

    finished 05_If_Bedingung

---
+++ Javascript/05_If_Bedingung/script.js
@@ -0,0 +1,12 @@
+let number = 20;
+
+if (number<20){
+    console.log("Mini");
+    
+} else if ((number>=20) && (number<=50)) {
+    console.log("Medium");
+    
+} else {
+    console.log("Large");
+    
+}
