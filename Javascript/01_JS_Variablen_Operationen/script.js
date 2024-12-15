f6235e6ab78b52b88097eb1baeff3e4387011631
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-12-15 16:13:58 +0100

    finished 01_JS_Variablen_&_Operationen

---
+++ Javascript/01_JS_Variablen_Operationen/script.js
@@ -0,0 +1,31 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+let result = a + b + c;
+console.log("Das Ergebnis von a + b + c ist: " + result);
+// a + b + c
+
+result = (a + b) / c;
+console.log("Das Ergebnis von (a + b) / c ist: " + result);
+// (a + b) / c
+
+result = c - a + b;
+console.log("Das Ergebnis von c - a + b ist: " + result);
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+a = 3
+b = a + 3
+c = c -3
+// gib die neuen Werte der Variablen aus
+
+console.log(a + ", " + b + ", " + c);
