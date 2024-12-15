f7c64a1f16ac6f77e26dfa6e434a578a5191ad22
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-12-15 16:40:13 +0100

    finished 03_Aufgabe_zu_Variablen

---
+++ Javascript/03_Aufgabe_zu_Variablen/script.js
@@ -0,0 +1,26 @@
+let banane = "Banane";
+let apple = "Apfel";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let averageWeightOfApple = 0.34;
+let averageWeightOfBanana = 0.22;
+
+let result = bananaPricePerKilo / averageWeightOfBanana;
+console.log(result);
+let price = bananaPricePerKilo / result;
+console.log(price);
+
+
+result = applePricePerKilo / averageWeightOfApple;
+console.log(result);
+price = applePricePerKilo / result;
+console.log(price);
+
+let priceOf8Apples = 8 * price
+console.log(priceOf8Apples);
+
+
+
+
