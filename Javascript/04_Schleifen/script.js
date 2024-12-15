3cf5cf4230e967e608e511abe796381f5c8ca882
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-12-15 17:28:21 +0100

    finihed 04_Schleifen

---
+++ Javascript/04_Schleifen/script.js
@@ -0,0 +1,16 @@
+for (let index = 0; index < 8; index++) {
+    console.log(index + 1 + " Elina");
+}
+
+for (let index = 7; index <= 45; index++) {
+    console.log(index);
+}
+
+for (let index = 10; index >= 0; index--) {
+    console.log(index);
+}
+
+for (let index = 0; index <= 10; index++) {
+    const element = array[index];
+    
+}
