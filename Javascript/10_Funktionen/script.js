82f9498f8a5e561b74792e7b6723ec691b6f4bd9
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-12-16 12:30:24 +0100

    finished 10_Funktionen

---
+++ Javascript/10_Funktionen/script.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a + b;
+}
+
+let result = add(5, 15);
+console.log(result);
+
+function subtract(a,b) {
+    return a - b;
+}
+
+let resultSubtraction = subtract(10, 2);
+console.log(resultSubtraction);
+
+
+function supercalculation(a,b){
+    return (a + b) / 2 * a
+}
+
+let resultSupercalculation = supercalculation(2, 8);
+console.log(resultSupercalculation);
