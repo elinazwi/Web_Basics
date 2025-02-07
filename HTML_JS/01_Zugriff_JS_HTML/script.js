ea392c33c595784842dc82ae0c88197fc43150f0
elina <e.zwischenbrugger@student.htldornbirn.at>
2025-02-07 09:38:18 +0100

    programmed with the videos

--- HTML_JS/01_Zugriff_JS_HTML/script.js
+++ HTML_JS/01_Zugriff_JS_HTML/script.js
@@ -1,3 +1,12 @@
-document.getElementById("content");
-element.innerHTML = "Tolle Sache"
-console.log(element);
+function buttonClicked(){
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) + parseFloat(num2);
+
+
+    console.log(result);
+    
+}
+
