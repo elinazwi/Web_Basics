ca387e5fe7509eecb4aafdfcac5ec6ac0eb47ec4
elina <e.zwischenbrugger@student.htldornbirn.at>
2025-02-24 10:53:14 +0100

    added football data to the website

---
+++ Javascript_Ajax/02_Football_API/script.js
@@ -0,0 +1,19 @@
+loadFootballTable();
+
+alert("hallo");
+
+function loadFootballTable() {
+    fetch('https://api.openligadb.de/getbltable/bl1/2024')
+      .then(response => response.json())
+      .then(json => {
+       console.log(json);
+       let html = "<div>";
+
+       for (let i = 0; i < json.length; i++) {
+        html += `<div>${json[i].teamName}</div>`;
+       }
+       html += "</div>";
+       document.getElementById("table").innerHTML = html;
+       
+    });
+}
