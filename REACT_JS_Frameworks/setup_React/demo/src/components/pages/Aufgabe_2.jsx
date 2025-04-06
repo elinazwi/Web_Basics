90c81ea29425f636e066bacbc7f4b15e60a6b749
elina <e.zwischenbrugger@student.htldornbirn.at>
2025-04-06 16:57:53 +0200

    added list

--- REACT_JS_Frameworks/setup_React/demo/src/components/pages/Aufgabe_2.jsx
+++ REACT_JS_Frameworks/setup_React/demo/src/components/pages/Aufgabe_2.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export default function Aufgabe2() {
+export default function List() {
   return (
     <>
     <h1 className='bg-amber-300'>Aufgabe2</h1>
       <div>
-        <h2 className='bg-amber-300'>Getränke</h2>
-        <div className='bg-pink-500'>Coffee</div>
-        <div className='bg-pink-500'>Tea</div>
-        <div className='bg-pink-500'>Beer</div>
+        <h2 className="p-5 border-2 border-white bg-pink-700 ml-3 w-50">Getränke:</h2>
+        <div className="p-5 border-2 border-white bg-pink-400 ml-3 hover:bg-pink-700 transition w-50">Coffee</div>
+        <div className="p-5 border-2  border-white bg-pink-400 ml-3 hover:bg-pink-700 transition w-50">Tea</div>
+        <div className="p-5 border-2 border-white bg-pink-400 ml-3 hover:bg-pink-700 transition w-50">Beer</div>
+
+        
       </div>
     </>
   )
